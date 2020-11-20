d196d3257e0ef8a7063a1014d6f2bb55dcc58f7a
Stanislao <stanislaoweb@libero.it>
2020-11-20 15:43:01 +0100

    Stampati gli indirizzi mail

--- main.js
+++ main.js
@@ -2,23 +2,30 @@
 // Dopo aver recuperato correttamente 10 indirizzi email e averli stampati in pagina in modo "grezzo",  date sfogo alla vostra creatività per rendere più accattivante la grafica della pagina
 
 
+
 var app = new Vue({
  el: '#root',
 
    data: {
-
+     mailArray: []
    },
+
    methods: {
 
    },
 
    mounted() {
+     
+     for (var i = 0; i < 10; i++) {
+       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
 
-     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(function (response) {
+          this.mailArray.push(response.data.response);
 
-        const result = response.data;
-    });
+          console.log(response.data.response);
+      });
+     }
+
 
    }
 });
